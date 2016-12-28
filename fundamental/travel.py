1934527089e0cdf7e62d2cf7289d4114bd2ee2aa
hshahriari@delphicdigital.com <hshahriari@delphicdigital.com>
2016-12-27 22:37:04 -0500

    Adds aircraft class to travel program

--- fundamental/travel.py
+++ fundamental/travel.py
@@ -17,3 +17,25 @@ class Flight:
 	def number(self):
 		print(self._number[:2])
 		return self._number
+
+	""" Get airline code """
+	def airline(number):
+		return self._number[:2]
+
+class Aircraft:
+
+	def __init__(self,registration,model,num_rows,num_seats_per_row):
+		self._registration = registration
+		self._model = model
+		self._num_rows = num_rows
+		self._num_seats_per_row = num_seats_per_row
+
+	def registration(self):
+		return self._registration
+
+	def model(self):
+		return self._model
+
+	def seating_plan(self):
+		return (range(1, self._num_rows + 1),
+				"ABCDEFGHJK"[:self._num_seats_per_row])
