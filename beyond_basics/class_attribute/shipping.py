ff80c1c26de5d123c40a46bd588456f96fa6d473
hshahriari@delphicdigital.com <hshahriari@delphicdigital.com>
2017-01-14 17:06:18 -0500

    Uses class method instead. ShippingContainer is passed as classmethod

--- beyond_basics/class_attribute/shipping.py
+++ beyond_basics/class_attribute/shipping.py
@@ -3,10 +3,10 @@ class ShippingContainer:
 
 	next_serial = 1337
 
-	@staticmethod
-	def _get_next_serial():
-		result = ShippingContainer.next_serial
-		ShippingContainer.next_serial += 1
+	@classmethod
+	def _get_next_serial(clas):
+		result = cls.next_serial
+		cls.next_serial += 1
 		return result
 
 	def __init__(self, owner_code, contents):
