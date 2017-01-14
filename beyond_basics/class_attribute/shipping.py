90d54a9c72f3b0a18e1655ce5a21830cad837f1d
hshahriari@delphicdigital.com <hshahriari@delphicdigital.com>
2017-01-14 17:16:17 -0500

    adds two methods that behave like contructor using classmethod

--- beyond_basics/class_attribute/shipping.py
+++ beyond_basics/class_attribute/shipping.py
@@ -4,10 +4,19 @@ class ShippingContainer:
 	next_serial = 1337
 
 	@classmethod
-	def _get_next_serial(clas):
+	def _get_next_serial(cls):
 		result = cls.next_serial
 		cls.next_serial += 1
 		return result
+
+	@classmethod
+	def creat_empty(cls, owner_code):
+		""" cls calling ShippingContainer contructor """
+		return cls(owner_code, contents=None)
+
+	@classmethod
+	def create_with_items(cls, owner_code, items):
+		return cls(owner_code, contents=list(items))
 
 	def __init__(self, owner_code, contents):
 		self.owner_code = owner_code
