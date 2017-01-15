e61a906935feecef323eefe80f3fa0fc018edafe
Hessam Shahriari <hessam61@gmail.com>
2017-01-14 20:49:29 -0500

    Extend refrigerated sub class constructor by adding temp

--- beyond_basics/class_attribute/shipping.py
+++ beyond_basics/class_attribute/shipping.py
@@ -14,13 +14,14 @@ class ShippingContainer:
 		return result
 
 	@classmethod
-	def creat_empty(cls, owner_code):
+	# adding *args, and **kwargs to support the "temp" attribute in refrigerated sub class
+	def creat_empty(cls, owner_code, *args, **kwargs):
 		""" cls calling ShippingContainer contructor """
-		return cls(owner_code, contents=None)
+		return cls(owner_code, contents=None, *args, **kwargs)
 
 	@classmethod
-	def create_with_items(cls, owner_code, items):
-		return cls(owner_code, contents=list(items))
+	def create_with_items(cls, owner_code, items, *args, **kwargs):
+		return cls(owner_code, contents=list(items), *args, **kwargs)
 
 	def __init__(self, owner_code, contents):
 		self.owner_code = owner_code
@@ -33,6 +34,16 @@ class ShippingContainer:
 
 class RefrigeratedShippingContainer(ShippingContainer):
 
+	MAX_TEMP = 4.0
+
 	@staticmethod
 	def _make_code(owner_code, serial):
 		return (owner_code+'R'+str(serial))
+
+
+	def __init__(self,owner_code, contents, temp):
+		# with super() we get a reference to the base class instance to extend the base class version
+		super().__init__(owner_code,contents)
+		if temp > RefrigeratedShippingContainer.MAX_TEMP:
+			raise ValueError("Temperature is too Hot!")
+		self.temp = temp
