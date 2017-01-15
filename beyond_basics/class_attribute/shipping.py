a8fdc8006ce0a1922ba5ce22cc3f43516b422e0d
Hessam Shahriari <hessam61@gmail.com>
2017-01-14 20:32:23 -0500

    Add a refrigerated sub class with different code

--- beyond_basics/class_attribute/shipping.py
+++ beyond_basics/class_attribute/shipping.py
@@ -1,11 +1,11 @@
 
 class ShippingContainer:
 
-	next_serial = 1337
+	next_serial = 1000
 
 	@staticmethod
 	def _make_code(owner_code, serial):
-		return (owner_code+str(serial))
+		return (owner_code+'U'+str(serial))
 
 	@classmethod
 	def _get_next_serial(cls):
@@ -25,6 +25,14 @@ class ShippingContainer:
 	def __init__(self, owner_code, contents):
 		self.owner_code = owner_code
 		self.contents = contents
-		self.code = ShippingContainer._make_code(
+		# use self instead of class name ShippingContainer for polymorphism, other wise in code U will be used
+		self.code = self._make_code(
 			owner_code,
 			ShippingContainer._get_next_serial())
+
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+	@staticmethod
+	def _make_code(owner_code, serial):
+		return (owner_code+'R'+str(serial))
