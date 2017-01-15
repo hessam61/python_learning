9d93c628682853fa4f4979e8561aa6b405a94129
Hessam Shahriari <hessam61@gmail.com>
2017-01-15 09:54:14 -0500

    Implement support for Fahrenheit property access

--- beyond_basics/class_attribute/shipping.py
+++ beyond_basics/class_attribute/shipping.py
@@ -14,8 +14,8 @@ class ShippingContainer:
 		return result
 
 	@classmethod
-	# adding *args, and **kwargs to support the "temp" attribute in refrigerated sub class
-	def creat_empty(cls, owner_code, *args, **kwargs):
+	# adding *args, and **kwargs to support the "temp_celsius" attribute in refrigerated sub class
+	def create_empty(cls, owner_code, *args, **kwargs):
 		""" cls calling ShippingContainer contructor """
 		return cls(owner_code, contents=None, *args, **kwargs)
 
@@ -34,27 +34,43 @@ class ShippingContainer:
 
 class RefrigeratedShippingContainer(ShippingContainer):
 
-	MAX_TEMP = 4.0
+	MAX_TEMP_CELSIUS = 4.0
 
 	@staticmethod
 	def _make_code(owner_code, serial):
 		return (owner_code+'R'+str(serial))
 
+	@staticmethod
+	def _c_to_f(temp_celsius):
+		return temp_celsius * 9/5 + 32
+	
+	@staticmethod
+	def _f_to_c(temp_fahrenheit):
+		return (temp_fahrenheit - 32) * 5/9
 
-	def __init__(self,owner_code, contents, temp):
+	def __init__(self,owner_code, contents, temp_celsius):
 		# with super() we get a reference to the base class instance to extend the base class version
 		super().__init__(owner_code,contents)
-		if temp > RefrigeratedShippingContainer.MAX_TEMP:
+		if temp_celsius > RefrigeratedShippingContainer.MAX_TEMP_CELSIUS:
 			raise ValueError("Temperature is too Hot!")
 		#not a part f public interface
-		self._temp = temp
+		self._temp_celsius = temp_celsius
 
 	@property
-	def temp(self):
-		return self._temp
+	def temp_celsius(self):
+		return self._temp_celsius
 
-	@temp.setter
-	def temp(self, value):
-		if value > RefrigeratedShippingContainer.MAX_TEMP:
-			raise ValueError ("Too hot of temperature for thsi container")
-		self._temp = value
+	@temp_celsius.setter
+	def temp_celsius(self, value):
+		if value > RefrigeratedShippingContainer.MAX_TEMP_CELSIUS:
+			raise ValueError ("Temperature is too Hot!")
+		self._temp_celsius = value
+
+	@property
+	def temp_fahrenheit(self):
+		return RefrigeratedShippingContainer._c_to_f(self.temp_celsius)
+
+	@temp_fahrenheit.setter
+	def temp_fahrenheit(self, value):
+		self.temp_celsius = RefrigeratedShippingContainer._f_to_c(value)
+
